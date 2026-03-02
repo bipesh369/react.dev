70f639a6b345454f7871af062d21b68017f5408f
bipesh369 <juniorbipesh@gmail.com>
2026-03-02 07:38:34 +0000

    learn about context api

---
+++ Projects/contextapi/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import UserContext  from './context/UserContext'
+import  UserContextProvider  from './context/UserContextProvider' 
+import './App.css'
+import Login from './components/Login'
+import Profile from './components/Profile'
+
+function App() {
+  
+
+  return (
+    <UserContextProvider>
+      <h1>Learning Context Api In React</h1>
+      <Login />
+      <Profile />
+    </UserContextProvider>
+  )
+}
+
+export default App
